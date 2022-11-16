d8702b5e146d5d0174dbd7a3671670791e379340
Guyon Matteau <guyon.matteau@gmail.com>
2022-11-16 18:41:11 +0100

    Lint

--- contracts/core/Scheduler.sol
+++ contracts/core/Scheduler.sol
@@ -9,24 +9,15 @@ import "@uniswap/v2-periphery/interfaces/IUniswapV2Router02.sol";
 import {DataTypes} from "contracts/libraries/DataTypes.sol";
 
 contract Scheduler {
-    event PaymentScheduled(
-        uint256 paymentId,
-        address indexed owner,
-        address indexed to,
-        uint256 amount
-    );
+    event PaymentScheduled(uint256 paymentId, address indexed owner, address indexed to, uint256 amount);
     event PaymentExecuted(uint256 indexed paymentId);
     event PaymentDeactivated(uint256 indexed paymentId, address indexed owner);
-    event TokenSupplied(
-        address indexed sender,
-        address indexed tokenAddress,
-        uint256 amount
-    );
+    event TokenSupplied(address indexed sender, address indexed tokenAddress, uint256 amount);
 
     uint256 public paymentId;
     address private immutable chainlinkRefContract;
     address private immutable weth;
-//    address private immutable usdc;
+    //    address private immutable usdc;
     //IUniswapV2Router02 private immutable router;
 
     // paymentId => payment
@@ -48,31 +39,24 @@ contract Scheduler {
     /// @notice Schedule a recurring payment at any day of the month
     /// @dev The day of the month is not yet converted
     /// @return The id of the scheduled recurring payment
-    function schedulePayment(
-        address to,
-        uint256 amount,
-        uint8 dayOfMonth
-    ) public returns (uint256) {
+    function schedulePayment(address to, uint256 amount, uint8 dayOfMonth) public returns (uint256) {
         require(dayOfMonth < 30, "dayOfMonth should be smaller than 30");
         paymentId++;
-        DataTypes.RecurringPayment memory newPayment = DataTypes
-            .RecurringPayment({
-                paymentId: paymentId,
-                owner: msg.sender,
-                to: to,
-                amount: amount,
-                dayOfMonth: dayOfMonth,
-                lastExecuted: 0,
-                isActive: true
-            });
+        DataTypes.RecurringPayment memory newPayment = DataTypes.RecurringPayment({
+            paymentId: paymentId,
+            owner: msg.sender,
+            to: to,
+            amount: amount,
+            dayOfMonth: dayOfMonth,
+            lastExecuted: 0,
+            isActive: true
+        });
         scheduledPayments[paymentId] = newPayment;
         emit PaymentScheduled(paymentId, msg.sender, to, amount);
         return paymentId;
     }
 
-    function getPaymentById(
-        uint256 id
-    ) public view returns (DataTypes.RecurringPayment memory) {
+    function getPaymentById(uint256 id) public view returns (DataTypes.RecurringPayment memory) {
         require(scheduledPayments[id].isActive, "Payment not found");
         return scheduledPayments[id];
     }
@@ -80,19 +64,12 @@ contract Scheduler {
     function executePayment(uint256 id) public {
         DataTypes.RecurringPayment memory payment = getPaymentById(id);
 
-        require(
-            tokenBalanceOf[payment.owner][weth] > payment.amount,
-            "User does not have sufficient funds in protocol"
-        );
+        require(tokenBalanceOf[payment.owner][weth] > payment.amount, "User does not have sufficient funds in protocol");
 
         tokenBalanceOf[msg.sender][weth] -= payment.amount;
         payment.lastExecuted = block.timestamp;
         scheduledPayments[id] = payment;
-        bool success = IERC20(weth).transferFrom(
-            address(this),
-            payment.to,
-            payment.amount
-        );
+        bool success = IERC20(weth).transferFrom(address(this), payment.to, payment.amount);
         require(success, "Transaction Failed");
 
         emit PaymentExecuted(id);
@@ -107,15 +84,11 @@ contract Scheduler {
     /// @dev This should do XA
     /// @param amount Amount of ETH to swap for USDC
     function swap(uint256 amount, address owner) internal {
-        
-        require(tokenBalanceOf[owner][weth] >= amount,
-                "Insufficient balance");
-       
+        require(tokenBalanceOf[owner][weth] >= amount, "Insufficient balance");
+
         // Pair WETH USDC
         //IUniswapV2Factory factory = IUniswapV2Factory(router.factory());
         //IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(weth, usdc));
-    
-
     }
 
     /// @notice Request current WETH/USDC rate needed to determine how much ETH should be swapped
@@ -124,10 +97,7 @@ contract Scheduler {
 
     function supply(uint256 amount) public {
         // check that user has enough funds
-        require(
-            IERC20(weth).balanceOf(msg.sender) >= amount,
-            "Insufficient balance"
-        );
+        require(IERC20(weth).balanceOf(msg.sender) >= amount, "Insufficient balance");
         // give scheduler approval to get funds from user
         require(IERC20(weth).approve(address(this), amount));
         // get funds from users
--- test/core/Scheduler.t.sol
+++ test/core/Scheduler.t.sol
@@ -7,10 +7,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Scheduler} from "contracts/core/Scheduler.sol";
 import {DataTypes} from "contracts/libraries/DataTypes.sol";
 
-
 contract SchedulerTest is Test {
     Scheduler internal scheduler;
-    
+
     ERC20 internal weth;
     ERC20 internal usdc;
     address user = address(100);
@@ -18,11 +17,10 @@ contract SchedulerTest is Test {
 
     // mainnet addresses to test with
     address internal constant _weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address internal constant _usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; 
+    address internal constant _usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address internal constant _uniswapRouter = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
     function setUp() public {
-        
         // create contract and provide it with 1 eth
         weth = ERC20(_weth);
         usdc = ERC20(_usdc);
@@ -31,19 +29,17 @@ contract SchedulerTest is Test {
             _usdc: _usdc,
             _uniswapRouter: _uniswapRouter
         });
-
     }
 
     /// @dev This is utility test to check if we can mint WETH on mainnet fork
     function testMintWETH() public {
-        
         vm.deal(user, 4 ether);
         assertEq(address(user).balance, 4 ether);
-        
+
         vm.startPrank(user);
-        
+
         // check that minting of weth is succesfull
-        (bool success, ) = address(weth).call{value: 2 ether}("");
+        (bool success,) = address(weth).call{value: 2 ether}("");
         assertTrue(success, "WETH mint not succesfull");
 
         uint256 wethBalanceOfUser = weth.balanceOf(user);
@@ -52,60 +48,51 @@ contract SchedulerTest is Test {
     }
 
     /// @notice Assert that a supplier can supply assets weth
-   function testSupplyWETH() public {
-
+    function testSupplyWETH() public {
         // first mint some WETH
         testMintWETH();
-    
+
         vm.startPrank(user);
         uint256 supplyAmount = 0.3 ether;
         bool approval = weth.approve({spender: address(scheduler), amount: 0.5 ether});
         assertTrue(approval, "WETH approval failed");
         scheduler.supply(supplyAmount);
 
-        uint256 suppliedAmount = scheduler.balanceOf({
-            user: user,
-            erc20: address(weth)
-        });
+        uint256 suppliedAmount = scheduler.balanceOf({user: user, erc20: address(weth)});
 
         assertEq(suppliedAmount, supplyAmount, "Supply to protocol not succesfull");
         vm.stopPrank();
     }
 
     /// @notice assert that payment is added
-    function testSchedulePayment(
-    ) public returns(uint256) {
-        
+    function testSchedulePayment() public returns (uint256) {
         vm.startPrank(user);
-        uint256 paymentId = scheduler.schedulePayment({to: to, amount: 0.2 ether, dayOfMonth: 2}); 
+        uint256 paymentId = scheduler.schedulePayment({to: to, amount: 0.2 ether, dayOfMonth: 2});
 
-        DataTypes.RecurringPayment memory payment = scheduler.getPaymentById(
-            paymentId
-        );
+        DataTypes.RecurringPayment memory payment = scheduler.getPaymentById(paymentId);
 
         emit log_named_uint("paymentId", payment.paymentId);
         return paymentId;
     }
 
     /// @notice assert that protocol can manually execute ERC20 transfer
-   function testExecutePayment() public {
-    
+    function testExecutePayment() public {
         // supply eth to contract (this is calling two other tests, not really best practice but fine for now)
         testSupplyWETH();
-    
+
         // schedule payment and get payment ID
         uint256 paymentId = testSchedulePayment();
-       
+
         uint256 wethBalanceOfTo = weth.balanceOf(to);
         assertEq(wethBalanceOfTo, 0, "WETH balance of recipient pre-transfer not 0");
-   
+
         // execute payment
         scheduler.executePayment(paymentId);
         uint256 newBalanceOfTo = weth.balanceOf(to);
         assertEq(newBalanceOfTo, 0.2 ether, "Recipient WETH balance post-transfer not as expected");
-        
+
         // post payment check
         uint256 newBalanceOfUser = scheduler.balanceOf(user, address(weth));
         assertEq(newBalanceOfUser, 0.1 ether, "New supply of user in protocl not as expected");
-   }
+    }
 }
